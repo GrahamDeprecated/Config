a02086856b9d66ec48c1e34ad36b3fffd7001792
Graham Campbell <graham@mineuk.com>
2015-03-23 16:35:13 +0000

    Added laravel fixers

--- src/Config.php
+++ src/Config.php
@@ -200,6 +200,67 @@ class Config
     ];
 
     /**
+     * The list of laravel fixers.
+     *
+     * @var string[]
+     */
+    protected static $laravelFixers = [
+        'psr0',
+        'encoding',
+        'short_tag',
+        'braces',
+        'elseif',
+        'eof_ending',
+        'function_call_space',
+        'function_declaration',
+        'indentation',
+        'line_after_namespace',
+        'linefeed',
+        'lowercase_constants',
+        'lowercase_keywords',
+        'method_argument_space',
+        'multiple_use',
+        'parenthesis',
+        'php_closing_tag',
+        'single_line_after_imports',
+        'trailing_spaces',
+        'visibility',
+        'concat_without_spaces',
+        'double_arrow_multiline_whitespaces',
+        'duplicate_semicolon',
+        'empty_return',
+        'extra_empty_lines',
+        'include',
+        'join_function',
+        'multiline_array_trailing_comma',
+        'namespace_no_leading_whitespace',
+        'no_blank_lines_after_class_opening',
+        'no_empty_lines_after_phpdocs',
+        'object_operator',
+        'operators_spaces',
+        'phpdoc_indent',
+        'phpdoc_no_package',
+        'phpdoc_short_description',
+        'phpdoc_to_comment',
+        'phpdoc_trim',
+        'phpdoc_type_to_var',
+        'phpdoc_var_without_name',
+        'remove_leading_slash_use',
+        'remove_lines_between_uses',
+        'return',
+        'single_array_no_trailing_comma',
+        'single_blank_line_before_namespace',
+        'spaces_before_semicolon',
+        'spaces_cast',
+        'standardize_not_equal',
+        'ternary_spaces',
+        'unused_use',
+        'whitespacy_lines',
+        'multiline_spaces_before_semicolon',
+        'short_array_syntax',
+    ];
+
+    /**
      * The list of recommended fixers.
      *
      * @var string[]
@@ -311,6 +372,9 @@ class Config
                 break;
             case 'symfony':
                 $this->fixers = static::$symfonyFixers;
+                break;
+            case 'laravel':
+                $this->fixers = static::$laravelFixers;
                 break;
             case 'recommended':
             case 'styleci':
