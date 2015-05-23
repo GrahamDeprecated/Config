0038a2836db23d5fd489eb17d05c9cd9cc984fb7
Graham Campbell <graham@cachethq.io>
2015-05-23 15:06:21 +0100

    Updated my email

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of StyleCI Config.
  *
- * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Graham Campbell <graham@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -18,7 +18,7 @@ use StyleCI\Config\Config;
 /**
  * This is the config test case class.
  *
- * @author Graham Campbell <graham@mineuk.com>
+ * @author Graham Campbell <graham@cachethq.io>
  * @author Joseph Cohen <joseph.cohen@dinkbit.com>
  */
 class ConfigTest extends AbstractTestCase
