88187aa6cd6d789cc04cf170a109277c2c3e2280
Graham Campbell <graham@cachethq.io>
2015-05-25 19:09:20 +0100

    Updated copyright

--- src/ConfigServiceProvider.php
+++ src/ConfigServiceProvider.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of StyleCI Config.
  *
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Cachet HQ <support@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
