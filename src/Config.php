fa7c8d7dff8fb12cc766ab2b1ec01af242159a24
Graham Campbell <graham@alt-three.com>
2015-08-01 00:12:50 +0100

    Updated the laravel preset

--- src/Config.php
+++ src/Config.php
@@ -256,6 +256,7 @@ class Config
         'linefeed',
         'line_after_namespace',
         'list_commas',
+        'logical_not_operators_with_successor_space',
         'lowercase_constants',
         'lowercase_keywords',
         'method_argument_space',
