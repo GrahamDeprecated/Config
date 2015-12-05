3bcc622be57f7d06a1b8457f3f0885c4a5f2aea9
Graham Campbell <graham@alt-three.com>
2015-12-05 15:21:00 +0000

    Tweak implementation to discard array keys

--- src/Config.php
+++ src/Config.php
@@ -546,15 +546,17 @@ class Config
             throw new InvalidPresetException($preset);
         }
 
-        if (!$this->allowRisky) {
-            foreach ($fixers as $key => $fixer) {
-                if (in_array($fixer, static::RISKY, true)) {
-                    unset($fixers[$key]);
+        if ($this->allowRisky) {
+            $this->fixers = $fixers;
+        } else {
+            $this->fixers = [];
+
+            foreach ($fixers as $fixer) {
+                if (!in_array($fixer, static::RISKY, true)) {
+                    $this->fixers[] = $fixer;
                 }
             }
         }
-
-        $this->fixers = $fixers;
 
         return $this;
     }
