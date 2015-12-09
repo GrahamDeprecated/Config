06ef89be6eec31c0e2dc0ea66de01a709febbf24
Graham Campbell <graham@alt-three.com>
2015-12-09 17:16:20 +0000

    Removed class_definition from the laravel preset

--- src/Config.php
+++ src/Config.php
@@ -286,7 +286,6 @@ class Config
         'array_element_white_space_after_comma',
         'blankline_after_open_tag',
         'braces',
-        'class_definition',
         'concat_without_spaces',
         'double_arrow_multiline_whitespaces',
         'duplicate_semicolon',
